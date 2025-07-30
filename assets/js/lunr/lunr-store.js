18a8b1ca3ad7cc9d2901d2ace008140ada2853f2
Varabho Bhikkhu <tudong@sumedharama.pt>
2025-07-30 18:39:14 +0200

    hola

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -11,7 +11,7 @@ var store = [{
       "categories": null,
       "date": null,
       "tags": null,
-      "url": "/https://varabhobhikkhu.github.io/archive",
+      "url": "/archive",
       "img": null
     },{
     "title": "Como llegar",
@@ -21,98 +21,8 @@ var store = [{
       "categories": null,
       "date": null,
       "tags": null,
-      "url": "/https://varabhobhikkhu.github.io/direccion",
+      "url": "/direccion",
       "img": null
-    },{
-    "title": "First post!",
-    "subtitle": "",
-    "excerpt": "This is my first post, how exciting!",
-    "content": "This is my first post, how exciting!",
-      "categories": [],
-      "date": "2015-01-04",
-      "tags": ["random","exciting-stuff"],
-      "url": "/https://varabhobhikkhu.github.io/20150104_first-post/",
-      "img": null
-    },{
-    "title": "Pirates arrrr",
-    "subtitle": "",
-    "excerpt": "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water or on a shore. It does not normally include crimes committed against persons traveling on the same vessel as the perpetrator (e.g. one passenger stealing from others on the same vessel). The term has been used throughout history to refer to raids across land borders by non-state agents.",
-    "content": "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water...",
-      "categories": [],
-      "date": "2015-01-15",
-      "tags": ["pirates","random"],
-      "url": "/https://varabhobhikkhu.github.io/20150115_pirates/",
-      "img": null
-    },{
-    "title": "Soccer",
-    "subtitle": "Best sport ever!",
-    "excerpt": "From Wikipedia:",
-    "content": "From Wikipedia: Association football, more commonly known as football or soccer,[2] is a sport played between two teams of eleven players with a spherical ball. It is played by 250...",
-      "categories": [],
-      "date": "2015-01-19",
-      "tags": [],
-      "url": "/https://varabhobhikkhu.github.io/20150119_soccer/",
-      "img": null
-    },{
-    "title": "Dear diary",
-    "subtitle": "",
-    "excerpt": "What is it with that Mary girl?  Dragging me to school every day. As if I had a choice.  What you don’t hear in those nursery rhymes is that she starves me if I don’t go to school with her; it’s the only way I can stay alive!  I’m thinking about being adopted by Little Bo Peep, sure I may get lost, but anything is better than being with Mary and those little brats at school (shudder, shudder).",
-    "content": "What is it with that Mary girl? Dragging me to school every day. As if I had a choice. What you don’t hear in those nursery rhymes is that she...",
-      "categories": [],
-      "date": "2015-01-27",
-      "tags": ["random","diary","school"],
-      "url": "/https://varabhobhikkhu.github.io/20150127_dear-diary/",
-      "img": null
-    },{
-    "title": "To be",
-    "subtitle": "… or not to be?",
-    "excerpt": "To be, or not to be–that is the question:Whether ‘tis nobler in the mind to sufferThe slings and arrows of outrageous fortuneOr to take arms against a sea of troublesAnd by opposing end them. To die, to sleep–No more–and by a sleep to say we endThe heartache, and the thousand natural shocksThat flesh is heir to. ‘Tis a consummationDevoutly to be wished. To die, to sleep–To sleep–perchance to dream: ay, there’s the rub,For in that sleep of death what dreams may comeWhen we have shuffled off this mortal coil,Must give us pause. There’s the respectThat makes calamity of so long life.For who would bear the whips and scorns of time,Th’ oppressor’s wrong, the proud man’s contumelyThe pangs of despised love, the law’s delay,The insolence of office, and the spurnsThat patient merit of th’ unworthy takes,When he himself might his quietus makeWith a bare bodkin? Who would fardels bear,To grunt and sweat under a weary life,But that the dread of something after death,The undiscovered country, from whose bournNo traveller returns, puzzles the will,And makes us rather bear those ills we haveThan fly to others that we know not of?Thus conscience does make cowards of us all,And thus the native hue of resolutionIs sicklied o’er with the pale cast of thought,And enterprise of great pitch and momentWith this regard their currents turn awryAnd lose the name of action. – Soft you now,The fair Ophelia! – Nymph, in thy orisonsBe all my sins remembered.",
-    "content": "To be, or not to be–that is the question: Whether ‘tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a...",
-      "categories": [],
-      "date": "2015-02-13",
-      "tags": [],
-      "url": "/https://varabhobhikkhu.github.io/20150213_hamlet-monologue/",
-      "img": null
-    },{
-    "title": "Test markdown",
-    "subtitle": "Each post also has a subtitle",
-    "excerpt": "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage.  I strongly encourage you to take 5 minutes to learn how to write in markdown - it’ll teach you how to transform regular text into bold/italics/headings/tables/etc.",
-    "content": "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage. I strongly encourage you to take 5 minutes to learn how to write in...",
-      "categories": [],
-      "date": "2015-02-20",
-      "tags": ["markdown"],
-      "url": "/https://varabhobhikkhu.github.io/20150220_test-markdown/",
-      "img": null
-    },{
-    "title": "Flake it till you make it",
-    "subtitle": "Excerpt from Soulshaping by Jeff Brown",
-    "excerpt": "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic reaction, no one would fault me if I threw it out. If I ended a relationship with a woman who hit me, no one would say that I had a commitment problem. But if I walk away from a seemingly secure route because my soul has other ideas, I am a flake?",
-    "content": "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic...",
-      "categories": [],
-      "date": "2015-02-26",
-      "tags": [],
-      "url": "/https://varabhobhikkhu.github.io/20150226_flake-it-till-you-make-it/",
-      "img": null
-    },{
-    "title": "Urban art",
-    "subtitle": "Creativity is everywhere",
-    "excerpt": "Urban artists can use anything as a canvas to express their art",
-    "content": "Urban artists can use anything as a canvas to express their art      Picture by Zarateman",
-      "categories": ["picture"],
-      "date": "2018-11-09",
-      "tags": ["urban art","street","downtown"],
-      "url": "/https://varabhobhikkhu.github.io/20181109_urban_art/",
-      "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Madrid_-_Graffiti_11.jpg/407px-Madrid_-_Graffiti_11.jpg"
-    },{
-    "title": "Dean Village",
-    "subtitle": "",
-    "excerpt": "Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and was the centre of a successful grain milling area for more than 800 years. At one time there were no fewer than eleven working mills there, driven by the strong currents of the Water of Leith.  From Wikipedia",
-    "content": "Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and...",
-      "categories": ["picture"],
-      "date": "2019-05-02",
-      "tags": ["landscape","trips","downtown","edinburgh","kramdown","bootstrap"],
-      "url": "/https://varabhobhikkhu.github.io/20190502_dean-village/",
-      "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Well_Court%2C_Dean_Village%2C_Edinburgh_%2844485996381%29.jpg/800px-Well_Court%2C_Dean_Village%2C_Edinburgh_%2844485996381%29.jpg"
     },{
     "title": "A landing page",
     "subtitle": "With image header",
@@ -121,7 +31,7 @@ var store = [{
       "categories": ["landing-page"],
       "date": "2019-08-03",
       "tags": ["example","demo","layout","guest-author"],
-      "url": "/https://varabhobhikkhu.github.io/20190803_landing-page/",
+      "url": "/20190803_landing-page/",
       "img": "https://dieghernan.github.io/chulapa/assets/img/site/transparent.png"
     },{
     "title": "The Acueduct of Segovia",
@@ -131,7 +41,7 @@ var store = [{
       "categories": ["picture"],
       "date": "2019-11-27",
       "tags": ["landscape","trips","downtown","project-links"],
-      "url": "/https://varabhobhikkhu.github.io/20191127_acueduct-of-segovia/",
+      "url": "/20191127_acueduct-of-segovia/",
       "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
     },{
     "title": "Current skin",
@@ -141,7 +51,7 @@ var store = [{
       "categories": ["skins"],
       "date": "2020-12-31",
       "tags": ["skin","bootstrap","current-theme","header-hero","image"],
-      "url": "/https://varabhobhikkhu.github.io/20201231_current-skin/",
+      "url": "/20201231_current-skin/",
       "img": "https://picsum.photos/id/1018/2000/2000"
     },{
     "title": "Caminante no hay camino",
@@ -151,7 +61,7 @@ var store = [{
       "categories": ["skins"],
       "date": "2025-05-06",
       "tags": ["skin","bootstrap","current-theme","header-hero","image"],
-      "url": "/https://varabhobhikkhu.github.io/20250506_caminante-no-hay-camino/",
+      "url": "/20250506_caminante-no-hay-camino/",
       "img": "https://picsum.photos/id/1018/2000/2000"
     },{
     "title": "Canticos",
@@ -161,7 +71,7 @@ var store = [{
       "categories": ["skins"],
       "date": "2025-05-21",
       "tags": ["skin","bootstrap","current-theme","header-hero","image"],
-      "url": "/https://varabhobhikkhu.github.io/20250521_canticos/",
+      "url": "/20250521_canticos/",
       "img": "https://picsum.photos/id/1018/2000/2000"
     },{
     "title": "Markdown cheatsheet",
@@ -171,7 +81,7 @@ var store = [{
       "categories": ["tutorial"],
       "date": "2025-07-30",
       "tags": ["starter","syntax","markdown"],
-      "url": "/https://varabhobhikkhu.github.io/cheatsheets/01-markdown-cheat-sheet",
+      "url": "/cheatsheets/01-markdown-cheat-sheet",
       "img": null
     },{
     "title": "kramdown cheatsheet",
@@ -181,6 +91,6 @@ var store = [{
       "categories": ["tutorial"],
       "date": "2025-07-30",
       "tags": ["starter","syntax","kramdown"],
-      "url": "/https://varabhobhikkhu.github.io/cheatsheets/02-kramdown-cheat-sheet",
+      "url": "/cheatsheets/02-kramdown-cheat-sheet",
       "img": null
     }]
